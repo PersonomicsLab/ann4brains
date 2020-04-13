b063e202c1908265f7d59fc96d872c151c865cd2
oadenekan123 <42779218+oadenekan123@users.noreply.github.com>
2020-04-12 20:46:56 -0400

    changed og brainnetcnn to py script

--- oyin/create_matrices.py
+++ oyin/create_matrices.py
@@ -2,6 +2,7 @@
 import numpy as np
 import os
 working_dir = '/home/oadenekan/src/cgan_norm/oyin'
+working_dir = r'C:\Users\oyina\src\senior_2019-2020\lab\bijsterbosch\project\oyin'
 os.chdir(working_dir)
 
 #%%
@@ -32,7 +33,8 @@ siteB = [list_[0] for list_ in siteB]
 siteB = [patient.split('\t') for patient in siteB]
 
 # type(siteB[0].split('\t'))
-
+len(siteB[0])
+len(siteB)
 #%% creating matrix
 siteB_pt1 = siteB[0]
 num_regions = 10
@@ -64,9 +66,10 @@ print(matrix[9])
 # %% creating matrix attempt part letter 2
 # upper triangular part
 test_dim = 5
+num_regions = 10
 counter = 0
-for row in range(test_dim):
-    for col in range(row+1, test_dim):
+for row in range(num_regions):
+    for col in range(row+1, num_regions):
         matrix[row][col] = siteB_pt1[counter]
         counter = counter + 1
         # print("row:", row)
@@ -75,10 +78,14 @@ for row in range(test_dim):
 print("sep")
 # lower triangular matrix
 counter = 0
-for col in range(test_dim):
-    for row in range(col+1, test_dim):
+for col in range(num_regions):
+    for row in range(col+1, num_regions):
         # matrix[row][]
         matrix[row][col] = siteB_pt1[counter]
         counter = counter + 1
         # print("col", col) 
         # print("row:", row)
+
+
+#%%
+np.array(matrix).shape
