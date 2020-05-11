cb78305d166df3964072092e11b4610ce0c1d0e4
oadenekan123 <42779218+oadenekan123@users.noreply.github.com>
2020-05-11 17:44:26 -0400

    file path issues

--- examples/BrainNet_CNN_JPT_test.py
+++ examples/BrainNet_CNN_JPT_test.py
@@ -184,7 +184,8 @@ print("mission completed!")
 # %%
 # Plot the training iterations vs. the training loss, the valid data mean-absolute-difference, 
 # and the valid data correlation with predicted and true (y_vald) labels.
-file_name = os.path.join(os.path.dirname(os.getcwd()), "/models/plot_metrics.png")
+file_name = os.getcwd() + "/models/plot_metrics.png"
+# file_name = os.path.join(os.getcwd(), name)
 E2Nnet_sml.plot_iter_metrics(True, file_name)
 
 # %%
